554fee203869eeea735fa50abadd6b0c6c3cffe8
jeroenouw <jeroenouw@outlook.com>
2018-07-29 12:21:04 +0200

    Deleted files

--- src/app/components/shared/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './profile.model';
-export * from './user.model';
-export * from './contact.model';
-export * from './phone-number.model';
--- src/app/components/shared/models/profile.model.ts
+++
@@ -1,6 +0,0 @@
-export class Profile {
-  displayName: string;
-  email: string;
-  bio: string;
-  photoUrl: string;
-}
